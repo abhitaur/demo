f9e1f312250a56aad07078e430d02713aa108e90
Abhishek taur <122015066+abhitaur@users.noreply.github.com>
2024-04-25 19:45:20 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home /> }/>
+        <Route path='/search' element={<Search /> }/>
+        <Route path='/watch/:id' element={<Watch /> }/>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+
+
---
+++ Search.tsx
@@ -0,0 +1,60 @@
+import { off } from "process";
+import React, { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useNavigate } from "react-router-dom";
+import Card from "../components/Card";
+import Navbar from "../components/Navbar";
+import SearchCard from "../components/SearchCard";
+import Sidebar from "../components/Sidebar";
+import Spinner from "../components/Spinner";
+import { clearVideos } from "../store";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { HomePageVideos } from "../Types";
+
+export default function Search() {
+  const navigate = useNavigate(); 
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  useEffect(() => {
+    dispatch(clearVideos());
+    if (searchTerm === "") navigate("/");
+    else {
+      dispatch(getSearchPageVideos(false));
+    }
+  }, [dispatch, navigate, searchTerm]);
+
+  return (
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+        {videos.length ? (
+          <div className="py-8 pl-8 flex flex-col gap-5 w-full">
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getSearchPageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={950}
+            >
+              {videos.map((item: HomePageVideos) => {
+                return (
+                  <div className="my-">
+                    <SearchCard data={item} key={item.videoId} />;
+                  </div>
+                );
+              })}
+            </InfiniteScroll>
+          </div>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    </div>
+  );
+}
